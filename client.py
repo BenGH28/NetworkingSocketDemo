a252854e2e5a92b5c0c022b3af4a147e747b6c4c
hunb2720 <ben.hunt@uleth.ca>
2019-11-24 16:08:25 -0700

    sends the quit signal to server

--- client.py
+++ client.py
@@ -9,14 +9,16 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 host = socket.gethostname()
 port = 8000
-server = "142.66.140.48"
+server= "142.66.140.47"#"142.66.140.48"
 s.connect((server, port))
 
 s.sendto("Hello World", (server, port))
 timer = threading.Timer(5.0, conError)
+timer.start()
 data, addr = s.recvfrom(1024)
 if data == "Hello Universe":
     print "Message: ", data
     s.sendto("Message Received", (server, port))
     data, addr = s.recvfrom(1024)
+    s.sendto("0", (server, port))
     print data

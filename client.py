59a6667f32997a5d879e5479d9edf90bcde0c75c
Austin (Austin) Kelly <kela2720@36thing.cs.uleth.ca>
2019-11-24 15:16:39 -0700

    Add primary client implementation

---
+++ client.py
@@ -0,0 +1,22 @@
+import socket
+import threading
+
+def conError():
+    print "connection error"
+    s.close()
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+host = socket.gethostname()
+port = 8000
+server = "142.66.140.48"
+s.connect((server, port))
+
+s.sendto("Hello World", (server, port))
+timer = threading.Timer(5.0, conError)
+data, addr = s.recvfrom(1024)
+if data == "Hello Universe":
+    print "Message: ", data
+    s.sendto("Message Received", (server, port))
+    data, addr = s.recvfrom(1024)
+    print data

dabb8cf6acdb208f7a60a3b23660d54e8bb20018
hunb2720 <ben.hunt@uleth.ca>
2019-11-30 12:55:03 -0700

    can resend on connecition issues

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 import socket
-import threading
 import struct
+import time
 
 def conError():
     print ("connection error")
@@ -8,44 +8,78 @@ def conError():
 
 def estConnection(port, host):
     address = (host,port)
+    resend = False
+    reRecv = False
+    count = 0
     #send 1
-    sent = sock.sendto(b'10', address)
-    print("sending {} bytes to {} ".format(sent,address))
+    while not resend and count < 3:
+        try:
+            sent = sock.sendto(b'10', address)
+            print("sending {} bytes to {} ".format(sent,address))
+            resend = True
+        except:
+            print("this didn't send #1")
+            count += 1
     #receive 1
-    data, addr = sock.recvfrom(1024)
+    count = 0
+    while not reRecv and count < 3 :
+        try:
+            data, addr = sock.recvfrom(1024)
+            reRecv = True
+        except:
+            print("this didn't receive #1")
+            count += 1
+    count = 0
     if data:
         print ("data received: ", data)
         #send 2
-        sent = sock.sendto(b'ack', address)
-        print("sending {} to {}".format(sent, address))
-        print("connection established")
-        return data
+        while True and count < 3:
+            try:
+                sent = sock.sendto(b'ack', address)
+                print("sending {} to {}".format(sent, address))
+                print("connection established")
+                return data
+            except:
+                print("this didn't send #2")
+                count += 1
+
     else:
         return None
 
-def disconnection(data, soc,addr):
-    if data == b'close':
-        soc.sendto(b'terminated', (server, port))
-        d, addr = soc.recvfrom(1024)
-        if d == b'okay':
-            print("Connection gracefully terminated")
-            return True
-    return False
-
+def disconnection(data, soc, addr):
+    try:
+        if data == b'close':
+            soc.sendto(b'terminated', (server, port))
+            d, addr = soc.recvfrom(1024)
+            if d == b'okay':
+                print("Connection gracefully terminated")
+                return True
+        return False
+    except:
+        pass
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.settimeout(2)
 host = socket.gethostname()
 port = 8000
-server= "142.66.140.56"#"142.66.140.48"
-data = estConnection(port, server)
+data = None
+# server = "142.66.140.47"
+try:
+    server= input("Enter the IP address of the server to connect to: ")
+    data = estConnection(port, server)
+except:
+    print("Not an active server address. Connection aborted.")
 
-while data:
-    data, addr = sock.recvfrom(1024) #recieve array
-    print("recieving: {} from {}".format(data,addr))
-    #send 3
-    # ack = 0
-    # sock.sendto(bytes(ack),(server, port))
-    # ack += 1
-    closed = disconnection(data, sock,addr)
-    if closed:
-        break
+while data is not None and data:
+    try:
+        data, addr = sock.recvfrom(1024) #recieve array
+        print("receiving: {} from {}".format(data,addr))
+        # ack = 0
+        # sock.sendto(bytes(ack),(server, port))
+        # ack += 1
+        closed = disconnection(data, sock,addr)
+        if closed:
+            break
+    except:
+        print("this didn't receive array")
+        break;

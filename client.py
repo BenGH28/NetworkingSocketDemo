b2678c079d84f0208366448642cb791137d19711
QuocHo0415 <48870844+QuocHo0415@users.noreply.github.com>
2019-11-30 13:08:12 -0700

    Add files via upload

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import socket
+import threading
 import struct
-import time
+import sys
 
 def conError():
     print ("connection error")
@@ -8,78 +9,90 @@ def conError():
 
 def estConnection(port, host):
     address = (host,port)
-    resend = False
-    reRecv = False
-    count = 0
     #send 1
-    while not resend and count < 3:
-        try:
-            sent = sock.sendto(b'10', address)
-            print("sending {} bytes to {} ".format(sent,address))
-            resend = True
-        except:
-            print("this didn't send #1")
-            count += 1
+    sent = sock.sendto(b'10', address)
+    print("sending {} bytes to {} ".format(sent,address))
     #receive 1
-    count = 0
-    while not reRecv and count < 3 :
-        try:
-            data, addr = sock.recvfrom(1024)
-            reRecv = True
-        except:
-            print("this didn't receive #1")
-            count += 1
-    count = 0
+    data, addr = sock.recvfrom(1024)
     if data:
         print ("data received: ", data)
         #send 2
-        while True and count < 3:
-            try:
-                sent = sock.sendto(b'ack', address)
-                print("sending {} to {}".format(sent, address))
-                print("connection established")
-                return data
-            except:
-                print("this didn't send #2")
-                count += 1
-
+        sent = sock.sendto(b'ack', address)
+        print("sending {} to {}".format(sent, address))
+        print("connection established")
+        return data
     else:
         return None
 
-def disconnection(data, soc, addr):
-    try:
-        if data == b'close':
-            soc.sendto(b'terminated', (server, port))
-            d, addr = soc.recvfrom(1024)
-            if d == b'okay':
-                print("Connection gracefully terminated")
-                return True
-        return False
-    except:
-        pass
+def disconnection(data, soc,addr):
+    if data == b'close':
+        soc.sendto(b'terminated', (server, port))
+        d, addr = soc.recvfrom(1024)
+        if d == b'okay':
+            print("Connection gracefully terminated")
+            return True
+    return False
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.settimeout(2)
+# Creates a packet from a sequence number and byte data
+def makePacket(seqNum, data = b''):
+    seqBytes = seqNum.to_bytes(4, byteorder = "little", signed = True)
+    return seqBytes + data
+
+# Extracts sequence number and data from a non-empty packet
+def extractPacket(packet):
+    seqNum = int.from_bytes(packet[0:4], byteorder = "little", signed = True)
+    return seqNum, packet[4:]
+
+
+# Receive packets from server
+def receive(socker, filename):
+	expectedNum = 0
+    file = open(filename, 'wb')
+    while True:
+        # Get the next packet from server
+        packet, addr = socket.recvfrom(bufferSize)
+        if not packet:
+            break
+        seqNum, data = extractPacket(packet)
+        print("Got packet", seqNum)
+
+        # Send back an ACK
+        if seqNum == expectedSeq:
+            print("Expected packet received")
+            print("Sending ACK", expectedNum)
+            packet = makePacket(expectedNum)
+            socket.sendto(packet, addr)
+            expectedNum += 1
+            file.write(data)
+        else:
+            print("Sending ACK", expectedNum - 1)
+            packet = makePacket(expectedNum - 1)
+			socket.sendto(packet, addr)
+    file.close()
+
+#To run
+bufferSize = 1024
 host = socket.gethostname()
 port = 8000
-data = None
-# server = "142.66.140.47"
-try:
-    server= input("Enter the IP address of the server to connect to: ")
-    data = estConnection(port, server)
-except:
-    print("Not an active server address. Connection aborted.")
+if __name__ == '__main__':
+    socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    socket.bind(host, port)
+    filename = sys.argv[0]
+    receive(socket, filename)
 
-while data is not None and data:
-    try:
-        data, addr = sock.recvfrom(1024) #recieve array
-        print("receiving: {} from {}".format(data,addr))
-        # ack = 0
-        # sock.sendto(bytes(ack),(server, port))
-        # ack += 1
-        closed = disconnection(data, sock,addr)
-        if closed:
-            break
-    except:
-        print("this didn't receive array")
-        break;
+#sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#host = socket.gethostname()
+#port = 8000
+#server= "142.66.140.56"#"142.66.140.48"
+#data = estConnection(port, server)
+
+#while data:
+#    data, addr = sock.recvfrom(1024) #recieve array
+#    print("recieving: {} from {}".format(data,addr))
+    #send 3
+    # ack = 0
+    # sock.sendto(bytes(ack),(server, port))
+    # ack += 1
+#    closed = disconnection(data, sock,addr)
+#    if closed:
+#        break
--- server.py
+++ server.py
@@ -4,15 +4,16 @@ import time
 import threading
 import struct
 import pickle
+import _thread
+import sys
+from timer import Timer
 
 def conError():
     print ("connection error")
     s.close()
 
-
 def estConnection(port, host):
         data, addr = sock.recvfrom(1024) # receive 1
-
         if data:
             print ("Received {} from {}".format(data,addr))
             sock.sendto(data, addr) # sending 1
@@ -31,19 +32,148 @@ def disconnection(data, sock, addr):
             print("Connection Closed")
             sock.close()
 
-array = []
-for i in range(20):
-    array.append(random.randint(1,1001))
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-data_array = pickle.dumps(array)
+#create data list
+def dataCreate():
+    for i in range (dataSize):
+            dataList.append(random.randint(1,1001))
 
-print("Awaiting connection...")
+#insert dataList to a file
+def fileCreate():
+    file = open("filename", "w")
+    dataCreate()
+    for i in dataList:
+        file.write(str(i) + "\n")
+    file.close()
 
-host = socket.gethostname()
+# Sets the window size
+def windowSizeSet(packetNums):
+    global num
+    return min(windowSize, packetNums - num)
+
+# Creates a packet from a sequence number and byte data
+def makePacket(seqNum, data = b''):
+    seqBytes = seqNum.to_bytes(4, byteorder = "little", signed = True)
+    return seqBytes + data
+
+# Extracts sequence number and data from a non-empty packet
+def extractPacket(packet):
+    seqNum = int.from_bytes(packet[0:4], byteorder = "little", signed = True)
+    return seqNum, packet[4:]
+
+# Receive thread
+def receive(socket):
+    global mutex
+    global num
+    global timer
+
+    while True:
+        packet, addr = socket.recvfrom(bufferSize)
+        packet, _ = packet, addr
+        ack, _ = extractPacket(packet)
+
+        # If we get an ACK for the first in-flight packet
+        print("Got ACK", ack)
+        if (ack >= num):
+            mutex.acquire()
+            num = ack + 1
+            print("num updated", num)
+            timer.stop()
+            mutex.release()
+
+#send using thread
+def send(socker, filename):
+    global mutex, num, timer
+    file = open(filename, 'rb')
+
+     # Add all the packet to the buffer
+    packet = []
+    seqNum = 0
+    while True:
+        data = file.read()
+        if not data:
+            break
+        packet.append(makePacket(seqNum, data))
+        seqNum += 1
+
+    packetSeq = len(packet)
+    print("Received", packetSeq)
+    windowSize = windowSizeSet(packetSeq)
+    sendNext = 0
+    num = 0
+
+    # Start the receiver thread
+    _thread.start_new_thread(receive, (socket,))
+
+    while num < packetSeq:
+        mutex.acquire()
+        # Send all the packet in window size
+        while sendNext < num + windowSize:
+            print("Sending packet", sendNext)
+            socket.sendto(packet[sendNext],clientAddress)
+            sendNext += 1
+
+        # Start timer
+        if not timer.running():
+            print("Start timer")
+            timer.start()
+
+        # Wait until timer goes off or get an ACK
+        while timer.running() and not timer.timeout():
+            mutex.release()
+            print("Sleeping")
+            time.sleep(timerSleep)
+            mutex.acquire()
+
+            if timer.timeout():
+                print("Timeout")
+                timer.stop();
+                sendNext = num
+            else:
+                print("Shifting window")
+                windowSize = windowSizeSet(packetSeq)
+        mutex.release()
+
+    file.close()
+
+timerSleep = 0.10
+timerTimeout = 5
+windowSize = 3
+bufferSize = 1024
+dataSize = 12
+dataList = []
 port = 8000
-sock.bind((host,port))
-address = estConnection(host, port)
-sock.sendto(b'close', address)
-data, addr = sock.recvfrom(1024)
-print(data)
-disconnection(data, sock, addr)
+
+#Resources for threading
+num = 0
+mutex = _thread.allocate_lock()
+timer = Timer(timerTimeout)
+
+
+#to run
+if __name__ == '__main__':
+
+
+    host = socket.gethostname()
+    clientAddress = (host, port)
+    serverAddress = (host, 0)
+    socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    socket.bind(serverAddress)
+    fileCreate()
+    filename = sys.argv[0]
+    send(socket, filename)
+
+
+#array = []
+#for i in range(20):
+#    array.append(random.randint(1,1001))
+#sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#data_array = pickle.dumps(array)
+
+
+#port = 8000
+#sock.bind((host,port))
+#address = estConnection(host, port)
+#sock.sendto(b'close', address)
+#data, addr = sock.recvfrom(1024)
+#print(data)
+#disconnection(data, sock, addr)

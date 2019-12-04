83c5e94da152d48b7047395a026cdf01c673a2d0
hunb2720 <ben.hunt@uleth.ca>
2019-12-04 14:44:15 -0700

    saving changes

--- client.py
+++ client.py
@@ -72,7 +72,7 @@ def receive(socket, filename):
 #To run
 bufferSize = 1024
 host = socket.gethostname()
-server = '142.66.140.52'
+server = 'localhost'
 port = 8000
 if __name__ == '__main__':
     socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
